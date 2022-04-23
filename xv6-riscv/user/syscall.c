7052a0b0b18e8d52032d2792a9b0d1fc372b74b7
Niv-1993 <nivdan7@gmail.com>
2022-04-23 17:46:33 +0300

    syscall user test

--- xv6-riscv/user/syscall.c
+++ xv6-riscv/user/syscall.c
@@ -1,12 +1,14 @@
-// #include "kernel/param.h"
-// #include "kernel/types.h"
-// #include "kernel/stat.h"
-// #include "user/user.h"
-// #include "kernel/fs.h"
-// #include "kernel/fcntl.h"
-// #include "kernel/syscall.h"
-// #include "kernel/memlayout.h"
-// #include "kernel/riscv.h"
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+#include "kernel/syscall.h"
+#include "kernel/memlayout.h"
+#include "kernel/riscv.h"
+
+
 
 // void pause_system_dem(int interval, int pause_seconds, int loop_size) {
 //     int pid = getpid();
@@ -92,17 +94,6 @@
 //     //kill_system_dem(10, 100);
 //     exit(0);
 // }
-
-
-#include "kernel/param.h"
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
-#include "kernel/fs.h"
-#include "kernel/fcntl.h"
-#include "kernel/syscall.h"
-#include "kernel/memlayout.h"
-#include "kernel/riscv.h"
 
 
 void env(int size, int interval, char* env_name) {
